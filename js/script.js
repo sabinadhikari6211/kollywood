06b142e14db1d229e15367f1b2b94345cefe8987
sabin <joker.sabin568@gmail.com>
2021-09-15 22:24:06 +0545

    completed

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('.user-upload-img').magnificPopup({
+      delegate: 'a', 
+      type: 'image',
+        gallery: {
+        enabled: true
+        }
+      
+    });
+    });
